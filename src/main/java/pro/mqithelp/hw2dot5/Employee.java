ab380c4397092885b200b8cb0d9e130ec5e843e2
MaxHome <mqithelp@gmail.com>
2024-08-28 20:25:05 +0300

    Перенес класс Employee

---
+++ src/main/java/pro/mqithelp/hw2dot5/Employee.java
@@ -0,0 +1,55 @@
+package pro.mqithelp.hw2dot5;
+
+import java.util.Objects;
+
+
+public class Employee {
+    private static int count;
+    private int id;
+    private final String surname;
+    private final String name;
+
+    public Employee(String surname, String name, String patronymic, int department, int salary) {
+        if (department < 1 || department > 5) {
+            department = 1;
+        }
+        this.name = name;
+        this.surname = surname;
+        id = count;
+        count++;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname));
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + getId() +
+                "\tФИО: " + surname + ' ' + name + '.';
+    }
+
+}
