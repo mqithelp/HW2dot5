b6d057ae686149f7f2a1c1de6d5834c92c4c2a3f
MaxHome <mqithelp@gmail.com>
2024-08-28 20:20:23 +0300

    init commit

---
+++ src/main/java/pro/mqithelp/hw2dot5/Hw2dot5Application.java
@@ -0,0 +1,13 @@
+package pro.mqithelp.hw2dot5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2dot5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw2dot5Application.class, args);
+    }
+
+}
